d33343cf228f0539474435580d8a64407f933817
Andrea <andrea.damico@gmail.com>
2010-02-24 09:19:50 +0100

    added spec and factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 Factory.define(:info) do |f|
   f.sequence(:name) { |n| "Info #{n}" }
+  f.phone_numbers { |phone_numbers| [phone_numbers.association(:phone_number)]}
+end
+
+Factory.define(:phone_number) do |f|
+  f.thenumber 303030
 end
 
