0ac573f0141a8cf59b1ecb96faaa1f29f2734678
Andrea <andrea.damico@gmail.com>
2010-02-24 09:19:28 +0100

    removed old erb templates and mongodb config

--- config/initializers/database.rb
+++
@@ -1,7 +0,0 @@
-MongoMapper.database = "infocap-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect_to_master if forked
-   end
-end
