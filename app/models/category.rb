4182f9627ed34297be4e30fdb5ccd1ecf58acf5f
Andrea <andrea.damico@gmail.com>
2010-02-25 11:15:00 +0100

    modified views, fixed attr accessible in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name, :new_theme_name
+  attr_accessible :name, :new_theme_name, :theme_id
   validates_presence_of :name
 
   has_many :infos
